8dfd79570b0817ef126e0f82dddff19ff3f7aabe
Bengisu Tepe <bengisutepe@hotmail.com>
2019-11-09 13:20:01 +0300

    MyInıt classında bir kullanıcı,10 kategori her kategoriye ait 10 ürün,10 özellik her özelliğe ait 10 ürün ve 10 order ve orderdetail fakedata ile yaratıldı.

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,34 @@
+﻿using Project.DAL.Strategy;
+using Project.MAP.Options;
+using Project.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext() : base("MyConnection")
+        {
+            Database.SetInitializer(new MyInit()); 
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+        public DbSet <OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Project.DAL/Strategy/MyInit.cs
@@ -0,0 +1,67 @@
+﻿using Project.DAL.Context;
+using Project.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Strategy
+{
+    public class MyInit : CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            AppUser user = new AppUser();
+            user.UserName = "YMS3424";
+            user.Password = "123";
+            context.AppUsers.Add(user);
+            context.SaveChanges();
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Category c = new Category();
+                c.CategoryName = FakeData.TextData.GetAlphabetical(10);
+                c.Description = FakeData.TextData.GetSentence();
+                context.Categories.Add(c);
+                context.SaveChanges();
+
+                EntityAttribute et = new EntityAttribute();
+                et.AttributeName = FakeData.TextData.GetAlphabetical(10);
+
+
+
+                for (int x = 0; x < 10; x++)
+                {
+                    Product pro = new Product();
+                    pro.ProductName = FakeData.TextData.GetAlphabetical(10);
+                    pro.UnitPrice = Convert.ToDecimal(FakeData.NumberData.GetDouble());
+                    pro.CategoryID = c.ID;
+                    context.Products.Add(pro);
+                    context.SaveChanges();
+
+                    ProductDetail pd = new ProductDetail();
+                    pd.AttributeID = et.ID;
+                    pd.ProductID = pro.ID;
+                    context.ProductDetails.Add(pd);
+                    context.SaveChanges();
+
+                    Order o = new Order();
+                    o.ShippedAddress= FakeData.TextData.GetAlphabetical(10);
+                    o.AppUserID = user.ID;
+                    context.Orders.Add(o);
+                    context.SaveChanges();
+
+                    OrderDetail od = new OrderDetail();
+                    od.ProductID = pro.ID;
+                    od.OrderID = o.ID;
+                    context.OrderDetails.Add(od);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
